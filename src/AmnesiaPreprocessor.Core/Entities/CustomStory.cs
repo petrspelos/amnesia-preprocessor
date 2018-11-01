7aec1e963994e0dad5f868346016adfe67bb6c67
Petr Sedláček <petr.spelos@gmail.com>
2018-11-01 16:00:54 +0100

    Create CustomStory entity

--- src/AmnesiaPreprocessor.Core/Entities/CustomStory.cs
+++ src/AmnesiaPreprocessor.Core/Entities/CustomStory.cs
@@ -1,16 +1,34 @@
+using System.IO;
 using System.Collections.Generic;
+using System.Collections.Immutable;
 
 namespace AmnesiaPreprocessor.Core.Entities
 {
     public class CustomStory
     {
         public string RootPath { get; private set; }
-        public List<string> SourceHpsFiles { get; private set; }
-        public List<string> IncludeHpsFiles { get; private set; }
+        public ImmutableList<string> SourceHpsFiles { get; private set; }
+        public ImmutableList<string> IncludeHpsFiles { get; private set; }
 
         public CustomStory(string rootPath)
         {
             RootPath = rootPath;
+            ThrowIfRootPathDoesNotExist();
+            LoadCustomStoryFiles();
+        }
+
+        private void ThrowIfRootPathDoesNotExist()
+        {
+            if(!Directory.Exists(RootPath))
+            { 
+                throw new DirectoryNotFoundException();
+            }
+        }
+
+        private void LoadCustomStoryFiles()
+        {
+            SourceHpsFiles = Directory.GetFiles(RootPath, "*.shps", SearchOption.AllDirectories).ToImmutableList();
+            IncludeHpsFiles = Directory.GetFiles(RootPath, "*.ihps", SearchOption.AllDirectories).ToImmutableList();
         }
     }
 }
