ba4de354743fa2cd9f3749f362251c55732af73b
Petr Sedláček <petr.spelos@gmail.com>
2018-11-01 15:39:03 +0100

    Initial commit

---
+++ src/AmnesiaPreprocessor.ConsoleApp/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace AmnesiaPreprocessor.ConsoleApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
---
+++ src/AmnesiaPreprocessor.Core/Class1.cs
@@ -0,0 +1,8 @@
+﻿using System;
+
+namespace AmnesiaPreprocessor.Core
+{
+    public class Class1
+    {
+    }
+}
