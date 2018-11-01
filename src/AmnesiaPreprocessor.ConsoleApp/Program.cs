767a2cbfbdd09ba2cc701101a813d08c99949722
Petr Sedláček <petr.spelos@gmail.com>
2018-11-01 19:25:08 +0100

    Implement ConsoleApp

--- src/AmnesiaPreprocessor.ConsoleApp/Program.cs
+++ src/AmnesiaPreprocessor.ConsoleApp/Program.cs
@@ -1,4 +1,8 @@
 ﻿using System;
+using System.IO;
+using System.Linq;
+using AmnesiaPreprocessor.Core.Directives;
+using AmnesiaPreprocessor.Core.Entities;
 
 namespace AmnesiaPreprocessor.ConsoleApp
 {
@@ -6,7 +10,48 @@ namespace AmnesiaPreprocessor.ConsoleApp
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            if(!args.Any(a => a.StartsWith("-cs:")))
+            {
+                Console.ForegroundColor = ConsoleColor.Red;
+                Console.WriteLine("You must provide the '-cs:' argument with the custom story path.");
+                Console.ResetColor();
+                return;
+            }
+
+            var csPath = args.First(a => a.StartsWith("-cs:")).Substring(4);
+
+            if(!Directory.Exists($@"{csPath}\maps"))
+            {
+                Console.ForegroundColor = ConsoleColor.Red;
+                Console.WriteLine($@"The path: '{csPath}\maps' does not exist!");
+                Console.ResetColor();
+                return;
+            }
+
+            var doNotMinify = args.Any(a => a == "--no-min");
+
+            var cs = new CustomStory(csPath);
+            
+            IDirective includeDirective = new IncludeDirective();
+            IDirective stringInterpolationDirective = new StringInterpolationDirective();
+            IDirective generatePreloadsDirective = new GeneratePreloadsDirective();
+            IDirective removeCommentsDirective = new RemoveCommentsDirective();
+            IDirective minifyCodeDirective = new MinifyCodeDirective();
+
+            includeDirective.Execute(cs);
+            stringInterpolationDirective.Execute(cs);
+            generatePreloadsDirective.Execute(cs);
+            removeCommentsDirective.Execute(cs);
+            
+            if(doNotMinify)
+            {
+                minifyCodeDirective.Execute(cs);
+            }
+
+            foreach(var compiledFile in cs.ProcessedFiles)
+            {
+                File.WriteAllText($@"{csPath}\maps\{compiledFile.Key}", compiledFile.Value);
+            }
         }
     }
 }
